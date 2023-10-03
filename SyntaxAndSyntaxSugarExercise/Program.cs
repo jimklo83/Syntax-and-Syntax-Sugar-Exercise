28ca0e5cf74dce71524668baa61d5f87873475d2
jimklo83 <jimklo83@mail.com>
2023-10-03 10:37:56 -0500

    Completed exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.IO.Pipes;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            Console.WriteLine((answer < 9)? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine"); 
+            
+            
         }
     }
 }
